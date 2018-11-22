1fe71cd62f80e6c804e84b84f8e6273ab15fbf06
solano <solanoepalacio@gmail.com>
2018-11-22 15:37:11 -0300

    moved routes to correct endpoint

--- src/routes/_game/index.js
+++ src/routes/_game/index.js
@@ -2,11 +2,68 @@
 
 const express = require('express');
 
-const loadRoutes = require('routes/loadRoutes');
+const route = express();
 
-const router = loadRoutes(__dirname);
+const GameModel = require('models/Game');
+const createGame = require('services/createGame');
+const checkPlayerMove = require('services/checkPlayerMove');
+
+// create game.
+route.post('/', function (req, res) {
+  const { body: { rows, cols } } = req;
+
+  if (!rows || !cols) {
+    return res.status(400).send('InvalidParams');
+  }
+
+  const game = createGame(rows, cols);
+  const gameData = GameModel.save(game);
+
+  res.status(200).send(
+    JSON.stringify(gameData)
+  );
+});
+
+const validActions = {
+  'sweep': true,
+  'flag': true
+};
+
+// update game;
+route.put('/', function (req, res) {
+  const { gameId, action, location } = req.body;
+  
+  if (
+    !gameId ||
+    !action ||
+    !location.x ||
+    !location.y ||
+    !validActions[action]
+  ) {
+    return res.status(400).send('InvalidParams')
+  }
+
+  const game = GameModel.find(gameId);
+  if (!game) {
+    res.status(404).send('GameNotFound');
+  } else if (game.status === 'finished') {
+    res.status(400).send(`Game ${gameId} is over.`);
+  }
+
+  let isGameOver = false;
+
+  if (action === 'sweep') {
+    isGameOver = checkPlayerMove(game, location);
+  }
+
+  const gameStatus = isGameOver ? 'finished' : 'playing';
+
+  GameModel.update(gameId, action, location, gameStatus);
+  
+  res.status(200).send(isGameOver);
+});
 
 module.exports = {
   prefix: '/game',
-  handler: router
+  handler: route
 }
