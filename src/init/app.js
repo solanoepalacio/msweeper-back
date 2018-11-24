3d94614eba3ff7b15d807fa2a4417ac807bc07e3
solano <solanoepalacio@gmail.com>
2018-11-24 11:44:16 -0300

    allowing every method on cors

--- src/init/app.js
+++ src/init/app.js
@@ -42,6 +42,7 @@ function errorMiddleware (error, req, res, next) {
 function corsMiddleware (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "*");
   next();
 }
 
